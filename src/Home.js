11aeda8c5e213dbe49d4c7e2e890fe30ef6e4c09
SAHIL-GARG-L\sahil.garg <sahil.garg@quovantis.com>
2021-10-14 19:06:08 +0530

    Implement Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        alert(`Hello Sahil!`);
+        console.log(e);
+    }
+
+    const handleClickAgain = (firstName, e) => {
+        alert(`Hello ${firstName}`);
+        console.log(e.target);
+    }
+
     return (
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Mario', e)}>Click Me Again</button>
         </div>
     );
 }
