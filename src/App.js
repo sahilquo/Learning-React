34e5936a35cee3837b0498b9cbce854e10e8a6cc
SAHIL-GARG-L\sahil.garg <sahil.garg@quovantis.com>
2021-10-14 18:51:48 +0530

    Applying css Styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import './App.css';
 import Navbar from './Navbar';
 import Home from "./Home";
 
 function App() {
-  const title = 'Welcome to the new Blog';
-  const likes = 50;
-  //const person = { name: 'Sahil', age: 27 };
-  const value = true;
-  const link = 'http://www.google.com';
 
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{value}</p>
 
-        <p>{Math.random()}</p>
-        <a href={link}>Google Site Link</a>
+      <div className="content">
+        <Home />
+
       </div>
     </div>
   );
