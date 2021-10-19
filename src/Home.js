888f03c97222e52f23efe983a6f780c27e24741f
SAHIL-GARG-L\sahil.garg <sahil.garg@quovantis.com>
2021-10-19 15:25:14 +0530

    Reusing Component

--- src/Home.js
+++ src/Home.js
@@ -6,13 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My First Blog', body: 'Lorem ipsum.....', author: 'Sahil', id: 1 },
         { title: 'My Second Blog', body: 'Lorem ipsummmm.....', author: 'Palvi', id: 2 },
-        { title: 'My Third Blog', body: 'Loremmmmm ipsum.....', author: 'Parag', id: 3 }
+        { title: 'My Third Blog', body: 'Loremmmmm ipsum.....', author: 'Sahil', id: 3 }
     ])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" />
-
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Sahil')} title="Sahil's Blogs" />
         </div >
     );
 }
